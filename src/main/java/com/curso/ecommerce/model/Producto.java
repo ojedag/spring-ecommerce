a11b293e8894f24039f129e436642a1bc69221fc
Ojeda Gregorio <ojeda.t.2000@gmail.com>
2025-05-15 03:33:45 -0500

    creación de la estuctura del proyecto y las clase modelo

---
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -0,0 +1,79 @@
+package com.curso.ecommerce.model;
+
+public class Producto {
+	private Integer id;
+	private String nombre;
+	private String descripcion;
+	private String image;
+	private double precio;
+	private int cantidad;
+	
+	public Producto() {
+	}
+
+	public Producto(Integer id, String nombre, String descripcion, String image, double precio, int cantidad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.image = image;
+		this.precio = precio;
+		this.cantidad = cantidad;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", image=" + image
+				+ ", precio=" + precio + ", cantidad=" + cantidad + "]";
+	}
+	
+	
+}
