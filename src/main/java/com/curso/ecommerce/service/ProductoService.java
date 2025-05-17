c5c76cdf23e2f92e670797a0bd8084e929b6cef7
Ojeda Gregorio <ojeda.t.2000@gmail.com>
2025-05-16 19:31:54 -0500

    funcionalidad mostrar productos para el administrador

--- src/main/java/com/curso/ecommerce/service/ProductoService.java
+++ src/main/java/com/curso/ecommerce/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -13,5 +14,5 @@ public interface ProductoService {
 	public Optional<Producto> get(Integer id);
 	public void update(Producto producto);
 	public void delete(Integer id);
-
+	public List<Producto> findAll();
 }
